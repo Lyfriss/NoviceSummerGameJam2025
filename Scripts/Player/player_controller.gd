d84eded23ab0393dc49c7ef422c616b78f5c565d
George M. Istratoaie <m.g.istratoaie@gmail.com>
2025-07-21 19:09:03 +0200

    Player v 01
    starting with simple smooth top down movement

--- Scripts/Player/player_controller.gd
+++ Scripts/Player/player_controller.gd
@@ -1,15 +1,25 @@
 extends CharacterBody2D
 
-@export var speed : float = 5.0
+@export_category("Debug")
+@export var debug: bool = false
 
+@export_category("Movement parameters")
+@export var speed : float = 100
+@export var acc: float = 100
+@export var dec: float = 450
 
 func _physics_process(delta: float) -> void:
 	
-	var direction := Input.get_vector("move_left","move_right","move_up","move_down")
+	if debug:
+		print(velocity)
+
+	var direction := Input.get_vector("move_left","move_right","move_up","move_down").normalized()
+	
 	if direction:
-		velocity = direction * speed
+		velocity.x =  clamp(velocity.x, lerp(velocity.x, direction.x * speed, acc * delta), direction.x * speed)
+		velocity.y = clamp(velocity.y, lerp(velocity.y, direction.y * speed, acc * delta), direction.y * speed)
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.y = move_toward(velocity.y, 0, speed)
+		velocity.x = move_toward(velocity.x, 0, dec * delta)
+		velocity.y = move_toward(velocity.y, 0, dec * delta)
 
 	move_and_slide()
